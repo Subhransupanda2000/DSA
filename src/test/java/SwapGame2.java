a5ab66fb32b86f3db710ff75a9381d676bc6918e
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-08 19:56:41 +0530

    SwapGame2

---
+++ src/test/java/SwapGame2.java
@@ -0,0 +1,13 @@
+import static junit.framework.TestCase.assertEquals;
+
+public class SwapGame2 {
+    static void myTestMethod()
+    {
+        String expectedOutPut= "A[10] \n" +
+                "B[20] \n" +
+                "B[20] \n" +
+                "A[10] \n";
+        assertEquals(expectedOutPut,SwapGame2.);
+    }
+    }
+
